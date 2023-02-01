c1e786b0ec6a0182d253380b435e207d0a70fb45
yoga-saputra <nyogasaputra_pro@alphaphilindo.com>
2023-02-01 10:47:59 +0800

    run without overseer

--- main.go
+++ main.go
@@ -1,25 +1,20 @@
 package main
 
-import (
-	"startup/routes"
-	"time"
-
-	"github.com/jpillora/overseer"
-	"github.com/jpillora/overseer/fetcher"
-)
+import "startup/routes"
 
 func main() {
-	overseer.Run(overseer.Config{
-		Program: gracefulStart,
-		Address: ":4000",
-		Fetcher: &fetcher.HTTP{
-			URL:      "http://localhost",
-			Interval: 1 * time.Second,
-		},
-		Debug: true,
-	})
+	routes.InitApi()
+	// overseer.Run(overseer.Config{
+	// 	Program: gracefulStart,
+	// 	Address: ":4000",
+	// 	Fetcher: &fetcher.HTTP{
+	// 		URL:      "http://localhost",
+	// 		Interval: 1 * time.Second,
+	// 	},
+	// 	Debug: true,
+	// })
 }
 
-func gracefulStart(state overseer.State) {
-	routes.InitApi(state)
-}
+// func gracefulStart(state overseer.State) {
+// 	routes.InitApi(state)
+// }
