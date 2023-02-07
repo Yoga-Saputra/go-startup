84670c29d2f201a6e6db6603af4599196cd34e66
yoga-saputra <nyogasaputra_pro@alphaphilindo.com>
2023-02-07 16:00:43 +0800

    Implement CI CD

---
+++ script.sh
@@ -0,0 +1,7 @@
+git pull origin main
+rm -rf /var/www/go-startup/startup
+export PATH=$PATH:/usr/local/go/bin
+go build .
+sudo service startup stop
+sudo service startup start
+systemctl daemon-reload
